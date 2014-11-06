2c04b5a7099e9e0c483434b8b2acaf602e9ecb01
Tom <tom@studiowolf.nl>
2014-11-06 16:00:10 +0100

    sigh

--- app/app.js
+++ app/app.js
@@ -1,9 +1,9 @@
 'use strict';
 
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname + '/app'));
-app.listen(process.env.PORT || 3000);
+// var express = require('express');
+// var app = express();
+// app.use(express.static(__dirname + '/app'));
+// app.listen(process.env.PORT || 3000);
 
 
 angular.module('FactsPerYearApp', [
