76d67f4a9c67e730e40db85bfb763be0c79d86a3
Tom <tom@studiowolf.nl>
2014-11-06 16:18:37 +0100

    stuff

--- app/app.js
+++ app/app.js
@@ -1,11 +1,5 @@
 'use strict';
 
-// var express = require('express');
-// var app = express();
-// app.use(express.static(__dirname + '/app'));
-// app.listen(process.env.PORT || 3000);
-
-
 angular.module('FactsPerYearApp', [
   'FactsPerYearApp.controllers',
   'FactsPerYearApp.services'
