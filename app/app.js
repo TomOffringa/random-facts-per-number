cd18eac64c36d0972b89a9c5b8adf424837f481f
Tom <tom@studiowolf.nl>
2014-11-06 14:36:25 +0100

    changed bower

--- app/app.js
+++ app/app.js
@@ -1,9 +1,10 @@
+'use strict';
+
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/app'));
 app.listen(process.env.PORT || 3000);
 
-'use strict';
 
 angular.module('FactsPerYearApp', [
   'FactsPerYearApp.controllers',
